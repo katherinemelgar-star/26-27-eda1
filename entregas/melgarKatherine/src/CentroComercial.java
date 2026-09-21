82acf563b9f5492a1e9e6d9c517ffa1b6c2797cf
katherinemelgar-star <katherine.melgar@alumnos.uneatlantico.es>
2026-09-21 11:31:16 +0000

    Corrige la duración de la simulación a 120 minutos y actualiza el reporte final para reflejar el cambio en el reto extendido

--- entregas/melgarKatherine/src/CentroComercial.java
+++ entregas/melgarKatherine/src/CentroComercial.java
@@ -3,7 +3,7 @@ public class CentroComercial {
     private int clientesAtendidos;
     private int minutoActual;
 
-    final private int MINUTOS_TOTALES = 240;
+    final private int MINUTOS_TOTALES = 120; 
     final private double PROB_LLEGADA = 0.6;
     final private double PROB_CAJA = 0.4;
 
@@ -16,22 +16,31 @@ public class CentroComercial {
     public void simular() {
         do {
             minutoActual++;
+    
             boolean llegaCliente = Math.random() < PROB_LLEGADA;
             int agregar = llegaCliente ? fila.recibir(new Persona()) : 0;
-
             boolean abreCaja = Math.random() < PROB_CAJA;
             boolean puedeAtender = abreCaja && fila.tieneGente();
-            
             clientesAtendidos = clientesAtendidos + (puedeAtender ? fila.despachar() : 0);
+            fila.actualizarTiempos();
+            boolean tocaRevisarAburridos = minutoActual >= 20 && minutoActual % 5 == 0;
+            int abandonos = tocaRevisarAburridos ? fila.purgarAburridos() : 0;
+
+            this.imprimirEstadoMinuto();
 
         } while (minutoActual < MINUTOS_TOTALES);
 
         this.mostrarReporte();
     }
 
+    private void imprimirEstadoMinuto() {
+        String dibujoFila = "-".repeat(fila.obtenerLongitud());
+        System.out.println(String.format("[%03d] Fila (%02dm): %s", minutoActual, fila.obtenerLongitud(), dibujoFila));
+    }
+
     private void mostrarReporte() {
         System.out.println("=".repeat(40));
-        System.out.println("REPORTE FINAL - RETO BASE (4 HORAS)");
+        System.out.println("REPORTE FINAL - RETO EXTENDIDO (2 HORAS)");
         System.out.println("=".repeat(40));
         System.out.println("Clientes atendidos: " + clientesAtendidos);
         System.out.println("Clientes que quedaron en fila: " + fila.obtenerLongitud());
