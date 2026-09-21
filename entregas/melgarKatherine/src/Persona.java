adcefa1b68b43ae5fee751d27188bc0737daee56
katherinemelgar-star <katherine.melgar@alumnos.uneatlantico.es>
2026-09-21 11:25:20 +0000

    Agrega la clase Persona con métodos para gestionar el tiempo de espera y determinar si se aburre

--- entregas/melgarKatherine/src/Persona.java
+++ entregas/melgarKatherine/src/Persona.java
@@ -1,4 +1,15 @@
 public class Persona {
+    private int minutosEsperando;
+
     public Persona() {
+        minutosEsperando = 0;
+    }
+
+    public void esperarMinuto() {
+        minutosEsperando++;
+    }
+
+    public boolean seAburre() {
+        return minutosEsperando > 8 && Math.random() < 0.3;
     }
 }
