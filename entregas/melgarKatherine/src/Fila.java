42b1cda44a4e390d07495d1327bea85f12ca7ed1
katherinemelgar-star <katherine.melgar@alumnos.uneatlantico.es>
2026-09-21 11:15:45 +0000

    Implementa la clase Fila con métodos para recibir, despachar y verificar la longitud de la fila

--- entregas/melgarKatherine/src/Fila.java
+++ entregas/melgarKatherine/src/Fila.java
@@ -1,3 +1,40 @@
 public class Fila {
-    
+
+    private Persona[] personas;
+    private int cantidad;
+    final private int CAPACIDAD_MAXIMA = 30; 
+
+    public Fila() {
+        personas = new Persona[CAPACIDAD_MAXIMA];
+        cantidad = 0;
+    }
+
+    public int recibir(Persona persona) {
+        boolean cabe = cantidad < CAPACIDAD_MAXIMA;
+        
+        if (cabe) {
+            personas[cantidad] = persona;
+            cantidad++;
+        }
+        
+        return cabe ? 1 : 0; 
+    }
+
+    public int despachar() {
+        assert cantidad > 0 : "Fila vacia no se puede despachar";
+        for (int i = 0; i < cantidad - 1; i++) {
+            personas[i] = personas[i + 1];
+        }
+        cantidad--;
+        personas[cantidad] = null; 
+        return 1; 
+    }
+
+    public boolean tieneGente() {
+        return cantidad > 0;
+    }
+
+    public int obtenerLongitud() {
+        return cantidad;
+}
 }
